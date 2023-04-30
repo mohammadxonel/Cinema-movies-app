93c497c9bb629fc4c72bcc85996482ccb616dd86
mohammadxxali <ali_mohammad@t-online.de>
2023-04-30 12:08:24 +0200

    add js app function get class by Rate

--- app.js
+++ app.js
@@ -51,3 +51,13 @@ function showMovies(movies) {
         main.appendChild(movieEl);
     });
 }
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
