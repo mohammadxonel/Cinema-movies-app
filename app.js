f78d99e90052f452e9ab820701c20d2bb8aa8923
mohammadxxali <ali_mohammad@t-online.de>
2023-05-01 12:38:50 +0200

    add js app function Event Listener

--- app.js
+++ app.js
@@ -61,3 +61,15 @@ function getClassByRate(vote) {
         return "red";
     }
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
